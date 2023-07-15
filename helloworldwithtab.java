1bc64fa251fd48f71ace611b9885a870e4a78555
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:50:23 +0530

    Add files via upload

---
+++ helloworldwithtab.java
@@ -0,0 +1,15 @@
+package Jayasrijavapractice;
+
+import java.util.Scanner;
+
+public class helloworldwithtab
+{
+
+	public static void main(String[] args)
+{
+		// TODO Auto-generated method stub
+		System.out.println("\tHello\tWorld\tHello\tWorld");
+
+	}
+
+}
